7f814ae84b8678a2c69325fa28a5016ae3d40f41
Sandip_Dutta_8 <ayandutta2005@gmail.com>
2024-07-23 21:48:40 +0530

    user schema created

--- api/index.js
+++ api/index.js
@@ -1,5 +1,15 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose';
+dotenv.config();
+
 const app = express();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("database connected");
+}).catch((err) => {
+    console.log(err)
+})
 
 app.listen(3000, ()=>{
     console.log('server running on port 3000!');
