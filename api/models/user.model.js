77d80da9eef5c693597ffa823cc5ebdddb30c5ea
Sandip_Dutta_8 <ayandutta2005@gmail.com>
2024-08-04 13:48:40 +0530

    google funtionality added

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -15,12 +15,12 @@ const userSchema = new mongoose.Schema(
         password: {
             type: String,
             required: true,
-        }
-        // profilePicture: {
-        //     type: String,
-        //     default:
-        //         'https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg',
-        // },
+        },
+        profilePicture: {
+            type: String,
+            default:
+                'https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg',
+        },
     },
     { timestamps: true }
 );
